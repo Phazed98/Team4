c2afbc14f63461741dc3b1a9f17d435a387219d4
Phazed98 <BattisonM@Gmail.com>
2015-02-03 12:03:25 +0000

    Fixed Camera
    Fixed Camera Class for Quality of Life changes, not going to be final
    Camera.

--- Game Tech 2014/nclgl/Camera.cpp
+++ Game Tech 2014/nclgl/Camera.cpp
@@ -1,58 +1,55 @@
 #include "Camera.h"
 
-/*
-Polls the camera for keyboard / mouse movement.
-Should be done once per frame! Pass it the msec since
-last frame (default value is for simplicities sake...)
-*/
-void Camera::UpdateCamera(float msec)	{
-	//Update the mouse by how much
-	pitch -= (Window::GetMouse()->GetRelativePosition().y);
-	yaw	  -= (Window::GetMouse()->GetRelativePosition().x);
+void Camera::UpdateCamera(float msec)
+{
+	pitch += (Window::GetMouse()->GetRelativePosition().y);
+	yaw += (Window::GetMouse()->GetRelativePosition().x);
 
-	//Bounds check the pitch, to be between straight up and straight down ;)
-	pitch = min(pitch,90.0f);
-	pitch = max(pitch,-90.0f);
+	pitch = min(pitch, 90.0f);
+	pitch = max(pitch, -90.0f);
 
-	if(yaw <0) {
-		yaw+= 360.0f;
-	}
-	if(yaw > 360.0f) {
+	if (yaw < 0)
+		yaw += 360.0f;
+
+	if (yaw > 360.0f)
 		yaw -= 360.0f;
-	}
 
-	msec *= 5.0f;
+	float speed = 2.0f;
 
-	if(Window::GetKeyboard()->KeyDown(KEYBOARD_W)) {
-		position += Matrix4::Rotation(yaw, Vector3(0,1,0)) * Vector3(0,0,-1) * msec;
-	}
-	if(Window::GetKeyboard()->KeyDown(KEYBOARD_S)) {
-		position -= Matrix4::Rotation(yaw, Vector3(0,1,0)) * Vector3(0,0,-1) * msec;
-	}
+	if (Window::GetKeyboard()->KeyDown(KEYBOARD_W))
+		position += Matrix4::Rotation(yaw, Vector3(0.0f, -1.0f, 0.0f)) * Matrix4::Rotation(pitch, Vector3(-1.0f, 0.0, 0.0f)) * Vector3(0.0f, 0.0f, -1.0f) * msec / speed;
 
-	if(Window::GetKeyboard()->KeyDown(KEYBOARD_A)) {
-		position += Matrix4::Rotation(yaw, Vector3(0,1,0)) * Vector3(-1,0,0) * msec;
-	}
-	if(Window::GetKeyboard()->KeyDown(KEYBOARD_D)) {
-		position -= Matrix4::Rotation(yaw, Vector3(0,1,0)) * Vector3(-1,0,0) * msec;
-	}
+	if (Window::GetKeyboard()->KeyDown(KEYBOARD_S))
+		position -= Matrix4::Rotation(yaw, Vector3(0.0f, -1.0f, 0.0f)) * Matrix4::Rotation(pitch, Vector3(-1.0f, 0.0, 0.0f)) * Vector3(0.0f, 0.0f, -1.0f) * msec / speed;
 
-	if(Window::GetKeyboard()->KeyDown(KEYBOARD_SHIFT)) {
-		position.y += msec;
-	}
-	if(Window::GetKeyboard()->KeyDown(KEYBOARD_SPACE)) {
-		position.y -= msec;
-	}
+	if (Window::GetKeyboard()->KeyDown(KEYBOARD_A))
+		position += Matrix4::Rotation(yaw, Vector3(0.0f, -1.0f, 0.0f)) * Vector3(-1.0f, 0.0f, 0.0f) * msec / speed;
+
+	if (Window::GetKeyboard()->KeyDown(KEYBOARD_D))
+		position -= Matrix4::Rotation(yaw, Vector3(0.0f, -1.0f, 0.0f)) * Vector3(-1.0f, 0.0f, 0.0f) * msec / speed;
+
+	if (Window::GetKeyboard()->KeyDown(KEYBOARD_SPACE))
+		position.y += msec / speed;
+
+	if (Window::GetKeyboard()->KeyDown(KEYBOARD_SHIFT))
+		position.y -= msec / speed;
+
+
 }
 
-/*
-Generates a view matrix for the camera's viewpoint. This matrix can be sent
-straight to the shader...it's already an 'inverse camera' matrix.
-*/
-Matrix4 Camera::BuildViewMatrix()	{
-	//Why do a complicated matrix inversion, when we can just generate the matrix
-	//using the negative values ;). The matrix multiplication order is important!
-	return	Matrix4::Rotation(-pitch, Vector3(1,0,0)) * 
-			Matrix4::Rotation(-yaw, Vector3(0,1,0)) * 
-			Matrix4::Translation(-position);
-};
+
+Vector3 Camera::getFacingVector()
+{
+	Matrix4 viewMatrix = BuildViewMatrix();
+
+	Vector3 faceVector(viewMatrix.values[2], viewMatrix.values[6], viewMatrix.values[10]);
+
+	return faceVector;
+}
+Matrix4 Camera::BuildViewMatrix()
+{
+	return	Matrix4::Rotation(-pitch, Vector3(-1, 0, 0)) *
+		Matrix4::Rotation(-yaw, Vector3(0, -1, 0)) *
+
+		Matrix4::Translation(-position);
+}
--- Game Tech 2014/nclgl/Camera.h
+++ Game Tech 2014/nclgl/Camera.h
@@ -1,60 +1,46 @@
-/******************************************************************************
-Class:Camera
-Implements:
-Author:Rich Davison	<richard.davison4@newcastle.ac.uk>
-Description:FPS-Style camera. Uses the mouse and keyboard from the Window
-class to get movement values!
-
--_-_-_-_-_-_-_,------,   
-_-_-_-_-_-_-_-|   /\_/\   NYANYANYAN
--_-_-_-_-_-_-~|__( ^ .^) /
-_-_-_-_-_-_-_-""  ""   
-
-*//////////////////////////////////////////////////////////////////////////////
 #pragma once
 
 #include "Window.h"
 #include "Matrix4.h"
 #include "Vector3.h"
+#include "Quaternion.h"
 
-class Camera	{
+class Camera
+{
 public:
-	Camera(void){
-		yaw		= 0.0f;
-		pitch	= 0.0f;
+	Camera(void)
+	{
+		yaw = 0.0f;
+		pitch = 0.0f;
 	};
+	Camera(float pitch, float yaw, Vector3 position)
+	{
+		this->pitch = pitch;
+		this->yaw = yaw;
+		this->position = position;
+	}
+	~Camera(void)
+	{
 
-	Camera(float pitch, float yaw, Vector3 position){
-		this->pitch		= pitch;
-		this->yaw		= yaw;
-		this->position	= position;
 	}
 
-	~Camera(void){};
-
 	void UpdateCamera(float msec = 10.0f);
-
-	//Builds a view matrix for the current camera variables, suitable for sending straight
-	//to a vertex shader (i.e it's already an 'inverse camera matrix').
+	Vector3 getFacingVector();
 	Matrix4 BuildViewMatrix();
+	Vector3 GetPosition() const		{ return position; }
+	void SetPosition(Vector3 val)	{ position = val; }
 
-	//Gets position in world space
-	Vector3 GetPosition() const { return position;}
-	//Sets position in world space
-	void	SetPosition(Vector3 val) { position = val;}
+	float GetYaw() const			{ return yaw; }
+	void SetYaw(float y)			{ yaw = y; }
 
-	//Gets yaw, in degrees
-	float	GetYaw()   const { return yaw;}
-	//Sets yaw, in degrees
-	void	SetYaw(float y) {yaw = y;}
+	float GetPitch() const			{ return pitch; }
+	void SetPitch(float p)			{ pitch = p; }
 
-	//Gets pitch, in degrees
-	float	GetPitch() const { return pitch;}
-	//Sets pitch, in degrees
-	void	SetPitch(float p) {pitch = p;}
 
 protected:
-	float	yaw;
-	float	pitch;
+
+	float yaw;
+	float pitch;
+	bool boost;
 	Vector3 position;
 };
