d5963eddcb054d0b5307f09383268c6ae0575e3b
Traldera <sam._white@hotmail.com>
2015-02-25 10:15:42 +0000

    Fixed DLL issue (you dont need to do anything!0

--- Game Tech 2014/Game Technologies/Game Technologies/GamePadController.h
+++ Game Tech 2014/Game Technologies/Game Technologies/GamePadController.h
@@ -14,7 +14,7 @@
 // Now, the XInput Library
 // NOTE: COMMENT THIS OUT IF YOU ARE NOT USING
 // A COMPILER THAT SUPPORTS THIS METHOD OF LINKING LIBRARIES
-#pragma comment(lib, "XInput.lib")
+#pragma comment(lib, "Xinput9_1_0.lib")
 
 // XBOX Controller Class Definition
 class GamePadController
